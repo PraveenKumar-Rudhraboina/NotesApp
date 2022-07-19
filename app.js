68e3b0b5e7e919ed42c8013b2ec24f34dee8ecca
praveen kumar Rudhraboina <praveenkumar@praveens-MacBook-Air.local>
2022-07-19 10:37:57 +0530

    node modules with package.json

--- app.js
+++ app.js
@@ -3,3 +3,6 @@ console.log(name1(1,2))
 
 const name2 = require('./notes.js')
 console.log(name2())
+
+const chalk  = require('chalk')
+console.log(chalk.yellow.italic.strikethrough.inverse.underline('Success!!'))
